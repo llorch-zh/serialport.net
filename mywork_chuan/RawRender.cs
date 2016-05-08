6605835e73e464ba127729725775c6c729dcdf61
llorch <cqwd2010@gmail.com>
2016-05-08 21:20:11 +0800

    RGB565 策略

--- mywork_chuan/RawRender.cs
+++ mywork_chuan/RawRender.cs
@@ -8,7 +8,7 @@ namespace mywork_chuan
 {
     class RawRender
     {
-        public void SimpleRGB(Bitmap bmp,byte[] data)
+        public void SimpleRGB(Bitmap bmp, byte[] data)
         {
             PointBitmap pb = new PointBitmap(bmp);
             pb.LockBits();
@@ -20,14 +20,28 @@ namespace mywork_chuan
                 int red = data[i] * 4 > 255 ? data[i] : 255;
                 int green = data[i + 1] * 4 > 255 ? data[i + 1] : 255;
                 int blue = red / 3 + green * 2 / 3;
-                //int red = (Global.DATA[i] & 0xF8) >> 3;
-                //int green = ((Global.DATA[i] & 0x07) << 3) | ((Global.DATA[i + 1] & 0xE0) >> 5);
-                //int blue = Global.DATA[i + 1] & 0x1F;
                 Color c = Color.FromArgb(red, green, blue);
                 pb.SetPixel(x, y, c);
             }
             pb.UnlockBits();
         }
 
+        public void RGB565(Bitmap bmp, byte[] data)
+        {
+            PointBitmap pb = new PointBitmap(bmp);
+            pb.LockBits();
+            for (int i = 0; i < data.Length; i += 2)
+            {
+                int x = (i % 640) / 2;
+                int y = i / 640;
+
+                int red = (data[i] & 0xF8) >> 3;
+                int green = ((data[i] & 0x07) << 3) | ((data[i + 1] & 0xE0) >> 5);
+                int blue = data[i + 1] & 0x1F;
+                Color c = Color.FromArgb(red, green, blue);
+                pb.SetPixel(x, y, c);
+            }
+            pb.UnlockBits();
+        }
     }
 }
